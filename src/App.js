2bb48ffdd2eb34720d6ab66c55fdcbf428ee1846
ad-fleming <adfleming@gmail.com>
2020-10-28 17:09:18 -0400

    added basic route to dummy About and Search pages. Updated path to be that of github pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Search from "./pages/Search";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <h1>My React App</h1>
-      <h2>Author: Alex Fleming</h2>
-    </>
+    <Router>
+      <>
+        <Route exact path="/react-employee-directory" component={About}/>
+        <Route exact path="/react-employee-directory/search" component={Search}/>
+      </>
+    </Router>
   );
 }
 
