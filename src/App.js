59506a36a64cb23e507036827ac4026102f899ff
ad-fleming <adfleming@gmail.com>
2020-10-28 17:27:17 -0400

    added beginning framework for navbar component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Search from "./pages/Search";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <>
         <Route exact path="/react-employee-directory" component={About}/>
+        <Route exact path="/react-employee-directory/about" component={About}/>
         <Route exact path="/react-employee-directory/search" component={Search}/>
       </>
     </Router>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to = "/react-employee-directory">
+                React Employee Search
+            </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link 
+                            to="/react-employee-directory"
+                            className={
+                                window.location.pathname === "/react-employee-directory" || window.location.pathname === "/react-employee-directory"
+                                ? "nav-link active"
+                                : "nav-link"
+                            }
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link 
+                            to="/react-employee-directory/search"
+                            className={
+                                window.location.pathname === "/react-employee-directory" ? "nav-link active" : "nav-link"}
+                        >
+                            Search
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
