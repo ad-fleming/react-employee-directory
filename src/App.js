73b6b1b8a25167892b5d68aa4db4b66f3db22e2e
ad-fleming <adfleming@gmail.com>
2020-10-28 22:40:15 -0400

    added components for col, container, footer, hero, navbar, row and wrapper

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <>
+        <Navbar />
         <Route exact path="/react-employee-directory" component={About}/>
         <Route exact path="/react-employee-directory/about" component={About}/>
         <Route exact path="/react-employee-directory/search" component={Search}/>
+        <Footer />
       </>
     </Router>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Hero from "../components/Hero/Hero"
 
 const About = () => {
     return (
         <div>
-            <h1>You made it to the About Page</h1>
+            <Hero backgroundImage="https://images.unsplash.com/photo-1450101499163-c8848c66ca85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80">
+                <h1>React Employee Search</h1>
+                <h2>We <strong><em>React</em></strong></h2>
+            </Hero>
         </div>
     );
 };
